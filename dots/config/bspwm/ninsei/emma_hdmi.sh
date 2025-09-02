5f5a64ee2dfd31b85f098f164650bff6e288a042
jelc53 <julian.e.cooper2@gmail.com>
2025-09-02 22:07:26 +0200

    tweak to monitor positioning

--- dots/config/bspwm/ninsei/emma_hdmi.sh
+++ dots/config/bspwm/ninsei/emma_hdmi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1200 --pos 0x1440 --rotate normal --output DP1 --off --output HDMI1 --mode 2560x1440 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 1920x1200 --pos 330x1440 --rotate normal --output DP1 --off --output HDMI1 --mode 2560x1440 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 bspc monitor eDP1 -d 1 3 4 5 6 7 8 9 10
 bspc monitor HDMI1 -d 2
